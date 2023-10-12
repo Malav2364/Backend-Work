f74a9c686bd16fb1eccd24ccd0ac1fe004100dcc
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-10-12 22:30:51 +0530

    listening boiler plate

--- backend2TAPI/server.js
+++ backend2TAPI/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Malav Patel is OP !')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`App Started on port ${port}`)
 })

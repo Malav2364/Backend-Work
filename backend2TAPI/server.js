ae744b2ad36278f5fc8056d6af6929d5ce185090
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-10-12 23:14:57 +0530

    ch

--- backend2TAPI/server.js
+++ backend2TAPI/server.js
@@ -1,11 +1,63 @@
-const express = require('express')
+import  express  from "express";
+
 const app = express();
-const port = 5000;
 
-app.get('/', (req, res) => {
-    res.send('Malav Patel is OP !')
-})
+app.get('/data', (req, res)=>{
+    const infoDta = [
+        {
+            id: 1,
+            name:"Malav Patel",
+            designation:"Developer",
+            age:19
+        },
+        {
+            id: 2,
+            name:"Yash Patel",
+            designation:"frontend",
+            age:20
+        },
+        {
+            id: 3,
+            name:"Rajesh Patel",
+            designation:"backend",
+            age:21
+        },
+        {
+            id: 4,
+            name:"Meet Patel",
+            designation:"marketing",
+            age:22
+        },
+        {
+            id: 5,
+            name:"Ayush Patel",
+            designation:"designer",
+            age:23
+        },
+        {
+            id: 6,
+            name:"Kamlesh Patel",
+            designation:"cleaner",
+            age:24
+        },
+        {
+            id: 7,
+            name:"Tony Patel",
+            designation:"manager",
+            age:25
+        },
+        {
+            id: 8,
+            name:"Mony Patel",
+            designation:"Employee",
+            age:26
+        }
+    ];
+    res.send(infoDta);
+});
 
-app.listen(port, () => {
-    console.log(`App Started on port ${port}`)
-})
+const port = process.env.PORT || 4000;
+
+app.listen(port, ()=>{
+    console.log(`App Served at http://localhost:${port}`)
+});
